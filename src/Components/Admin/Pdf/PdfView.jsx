5e97ba3a70b0b2bf21e7765581995d3b650e2883
HarshCodeCraft <harshuharshu609@gmail.com>
2025-06-17 19:48:05 +0530

    pdfview

--- src/Components/Admin/Pdf/PdfView.jsx
+++ src/Components/Admin/Pdf/PdfView.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-// import React, { useEffect } from "react";
+// import React from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import axios from "axios";
 import { showAlert } from "../../utils/ShowAlert";
