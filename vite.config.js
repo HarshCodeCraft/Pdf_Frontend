4c580d42d9078275f45bc57855ec021201bf9492
HarshCodeCraft <harshuharshu609@gmail.com>
2025-07-18 16:47:41 +0530

    change back in vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+   base: '/',
   plugins: [react()],
-  server: {
-    host: '127.0.0.1',
-    proxy: {
-      '/.well-known': {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
-        rewrite: pathStr => pathStr // <- Don't remove this
-      }
-    }
-  }
 })
