d12b6a32ee0f0afb4a305e156a8b4e8a0490106a
HarshCodeCraft <harshuharshu609@gmail.com>
2025-07-19 16:29:03 +0530

    fix issue i think

--- src/Components/Admin/Pdf/Pdf.jsx
+++ src/Components/Admin/Pdf/Pdf.jsx
@@ -12,7 +12,7 @@ const Pdf = () => {
     const fetchPdfData = async () => {
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_BASE_URL_API}/pdf/details`
+          `${import.meta.env.VITE_BASE_URL_API}/pdf/view`
         );
         setPdfData(response.data.data.reverse());
         console.log(response.data.data);
